7f1c34135911e76bf3ea14dfb07b2224ab69de6c
Jefferson <jefferson.boldrin@gmail.com>
2019-05-28 00:15:41 -0300

    First version of the gh-pages sample project

--- projects/ngx-scale/src/public_api.ts
+++
@@ -1,6 +0,0 @@
-/*
- * Public API Surface of ngx-scale
- */
-
-export * from './lib/ngx-scale.module';
-export * from './lib/ngx-scale-container.directive';
--- src/app/sample/sample-auto-resize/sample-auto-resize.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-
-@Component({
-  selector: 'app-sample-auto-resize',
-  templateUrl: './sample-auto-resize.component.html',
-  styleUrls: ['../sample.component.scss'],
-})
-export class SampleAutoResizeComponent { }
